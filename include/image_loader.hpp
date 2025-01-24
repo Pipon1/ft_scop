e5d977837768e6ff1ddb2fa7052d551cbeb70970
Pipon <112019293+Pipon1@users.noreply.github.com>
2025-01-24 15:48:05 +0100

    Added shader + image loader

---
+++ include/image_loader.hpp
@@ -0,0 +1,12 @@
+#ifndef IMAGE_LOADER_HPP
+# define IMAGE_LOADER_HPP
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+GLuint LoadBMP(const char *filename);
+
+#endif
